ac581e7a10e05ea2e1747c9c06be1ae055be7b0b
Rody Davis <rody.davis.jr@gmail.com>
2022-01-25 12:20:09 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/vite-lit-capacitor/",
   build: {
     rollupOptions: {
       input: "index.html",
