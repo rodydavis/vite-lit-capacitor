e022247a0f9e3a1d04afd5334e2ce16846cc0ce6
rodydavis <rodydavis@users.noreply.github.com>
2022-01-25 20:21:15 +0000

    deploy: ac581e7a10e05ea2e1747c9c06be1ae055be7b0b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/vite-lit-capacitor/sw.js', { scope: '/vite-lit-capacitor/' })})}
